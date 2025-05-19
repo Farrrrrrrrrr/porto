200f998c8fa37e80a4bfb480d3053ce903c96897
Farrell Ivander Daviano Siwy <farrell_ivander@Farrells-MacBook-Pro.local>
2025-05-20 04:04:41 +0700

    sixteenth commit

--- next.config.js
+++ next.config.js
@@ -7,43 +7,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Disable PostCSS processing to troubleshoot build issues
-  experimental: {
-    esmExternals: true
-  },
+  // Remove the direct require of tailwindcss
   webpack: (config) => {
-    // Force CSS resolution
-    config.module.rules.forEach((rule) => {
-      const { oneOf } = rule;
-      if (oneOf) {
-        oneOf.forEach((one) => {
-          if (!one.issuer && one.use && one.use.length > 0) {
-            one.use.forEach((u) => {
-              if (u.loader && u.loader.includes('css-loader') && !u.loader.includes('postcss-loader')) {
-                u.options = {
-                  ...u.options,
-                  importLoaders: 1,
-                };
-              }
-              if (u.loader && u.loader.includes('postcss-loader')) {
-                u.options = {
-                  ...u.options,
-                  postcssOptions: {
-                    plugins: [
-                      require('tailwindcss'),
-                      require('autoprefixer'),
-                    ],
-                  },
-                };
-              }
-            });
-          }
-        });
-      }
-    });
-    
     return config;
   },
+  // Simplify the build process
+  swcMinify: true,
+  output: 'standalone'
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
